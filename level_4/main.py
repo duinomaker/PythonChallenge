021583ac27b1ce13a876b7480cc97693165282cd
duinomaker <1318388396@qq.com>
2019-06-19 14:03:48 +0800

    Level 4 completed.

---
+++ level_4/main.py
@@ -0,0 +1,11 @@
+# http://www.pythonchallenge.com/pc/def/equality.html
+
+import re
+
+pattern = re.compile(r'[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]')
+
+with open('level_4/mess') as f:
+    mess = f.read().replace('\n', '')
+
+result = pattern.findall(mess)
+print(''.join(result))
