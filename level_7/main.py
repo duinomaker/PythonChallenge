ba215e489da76fe9deec4ad248fd420c81ab98c4
duinomaker <1318388396@qq.com>
2019-06-19 15:27:41 +0800

    Level 7 completed.

---
+++ level_7/main.py
@@ -0,0 +1,19 @@
+# http://www.pythonchallenge.com/pc/def/oxygen.html
+
+import PIL.Image as Image
+img = Image.open('level_7/oxygen.png')
+
+pixels = []
+
+last_index = 0
+for i in range(0, 629, 7):
+    r, g, b, a = img.getpixel((i, 47))
+    if not (r == g == b):
+        break
+    pixels.append(r)
+
+text_1 = ''.join(chr(i) for i in pixels)
+print(text_1)
+
+text_2 = ''.join(chr(i) for i in [105, 110, 116, 101, 103, 114, 105, 116, 121])
+print(text_2)
