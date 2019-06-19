7d57886c29d55fcf4479bfa241f7d947b8834aa1
duinomaker <1318388396@qq.com>
2019-06-19 15:11:28 +0800

    Level 6 completed.

---
+++ level_6/main.py
@@ -0,0 +1,31 @@
+# http://www.pythonchallenge.com/pc/def/channel.html
+# Please extract archive 'channel.zip' before running this
+
+import os
+import zipfile
+
+def extract(text):
+    result = []
+    for c in text:
+        if c.isdigit():
+            result.append(c)
+    return ''.join(result)
+
+os.chdir('level_6/channel')
+nothing = '90052'
+
+zfile = zipfile.ZipFile('../channel.zip')
+comments = b''
+
+while nothing:
+    filename = '%s.txt' % (nothing,)
+    comments += zfile.getinfo(filename).comment
+    with open(filename) as f:
+       text = f.read()
+       print(text)
+    if 'Next nothing is ' in text and text.index('Next') == 0:
+        nothing = extract(text)
+    else:
+        nothing = input('Enter your judgement: ')
+
+print(comments.decode())
