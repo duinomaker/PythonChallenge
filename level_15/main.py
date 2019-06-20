875a7b1e48fb24fbbf01f140c0d77bfe6a05f412
duinomaker <1318388396@qq.com>
2019-06-20 21:15:24 +0800

    Level 15 completed.

---
+++ level_15/main.py
@@ -0,0 +1,10 @@
+import datetime
+
+eligible = []
+for i in range(1006, 1997):
+    if i % 10 == 6 and (i % 400 == 0 or (i % 4 == 0 and i % 100 != 0)):
+        if datetime.datetime(i, 1, 1).isoweekday() == 4:
+            eligible.append(i)
+
+print(eligible)
+# Wolfgang Amadeus Mozart, born in 26/1/1756
