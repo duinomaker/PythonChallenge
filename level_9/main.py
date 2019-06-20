978632a36869678342bfc188dde43cc2ea222979
duinomaker <1318388396@qq.com>
2019-06-20 09:11:53 +0800

    Level 9 complete.

---
+++ level_9/main.py
@@ -0,0 +1,19 @@
+# http://www.pythonchallenge.com/pc/return/good.html (huge:file)
+
+from PIL import Image, ImageDraw
+import pickle
+
+with open('level_9/first', 'rb') as f:
+    first = pickle.load(f)
+
+with open('level_9/second', 'rb') as f:
+    second = pickle.load(f)
+
+img = Image.new('L', (480, 480))
+drawer = ImageDraw.Draw(img)
+
+drawer.polygon(first, fill=210)
+drawer.polygon(second, fill=230)
+
+img.save('level_9/result.bmp')
+img.show()
